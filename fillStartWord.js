4a09f7e34094c81b99be2255bf643575163c7f34
Tanyathip Aoongern <Tanyathip.aoongern@mail.kmutt.ac.th>
2022-09-05 20:17:11 +0700

    Homework fillStartWord-1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,23 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+
+  if(word == null) {
+      console.log("undefined")
+      return undefined
+  }
+  else if(word == undefined) {
+      console.log("undefined")
+      return undefined
+  }
+  else if (word.includes(startWord)) {
+      console.log(word)
+      return word
+  }
+  else  {
+      console.log(word=startWord+word)
+      return word=startWord+word
+
+  }    
 }
 module.exports = fillStartWord
